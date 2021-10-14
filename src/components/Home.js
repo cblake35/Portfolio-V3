d9bad1f1da50f43c1806ad4de8c94111689cfc98
Chris Blake <cblake2335@gmail.com>
2021-10-13 22:16:16 -0400

    Initiated initial file structure.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+function Home() {
+
+
+    return (
+        <Container>
+            <h1>Start project here.</h1>
+        </Container>
+    )
+}
+
+export default Home
