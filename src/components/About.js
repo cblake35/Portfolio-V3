08f5288c627ee1b5ac1cace3d2ecf58858da40e6
Chris Blake <cblake2335@gmail.com>
2021-11-01 20:14:05 -0400

    Updated about me section. Added download button with animation for CV and added skill icons in skills section.

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import profileImg from '../assets/profileImg.jpg';
+import { IoMdDownload } from 'react-icons/io';
+import resume from '../assets/CBResume.pdf'
+import { FaReact } from 'react-icons/fa';
+import { SiPostgresql } from 'react-icons/si';
+import { SiExpress } from 'react-icons/si';
+import { FaNodeJs } from 'react-icons/fa';
+import { AiFillHtml5 } from 'react-icons/ai';
+import { FaCss3Alt } from 'react-icons/fa';
+import { SiTypescript } from 'react-icons/si';
+import { FaBootstrap } from 'react-icons/fa';
+import { SiSequelize } from 'react-icons/si';
+import { SiPostman } from 'react-icons/si';
+import { IoLogoJavascript } from 'react-icons/io'
 
 function About() {
 
@@ -13,15 +26,19 @@ function About() {
             </Row>
             <Row>
                 <Col className='aboutMeImg'>
-                    <img src={profileImg} alt='Im Chris'/>
+                    <img src={profileImg} alt='Im Chris' />
                 </Col>
                 <Col className='aboutMeInfo'>
                     <h2>Personal Info</h2>
                     <p>Hello! My name is Chris and I enjoy solving problems and creating business solutions in the web.
-                        I've always been curious and intrigued on how software can assist us in our daily lives. Technology nowadays is a 
+                        I've always been curious and intrigued on how software can assist us in our daily lives. Technology nowadays is a
                         huge part of our livelihood and as an avid nerd, I want to contribute to make the world
                         a better place, one web app at a time.
                     </p>
+                    <a href={resume} download>
+                        <span className='resumeDownload'>Download Resume</span>
+                        <span className='resumeSvg'><IoMdDownload /></span>
+                    </a>
                 </Col>
             </Row>
             <Row className='skillsTitleWrapper'>
@@ -32,8 +49,49 @@ function About() {
                 </Col>
             </Row>
             <Row>
-                <Col className='skillsList'>
-                    list all relevant skills here
+                <Col md={3} className='skillsList'>
+                    <FaReact />
+                    <h5>React</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <IoLogoJavascript />
+                    <h5>JavaScript</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <SiPostgresql />
+                    <h5>Postgres</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <SiTypescript />
+                    <h5>TypeScript</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <FaNodeJs />
+                    <h5>Node</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <SiExpress />
+                    <h5>Express</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <AiFillHtml5 />
+                    <h5>HTML</h5>
+                </Col>
+                <Col md={3} className='skillsList'>
+                    <FaCss3Alt />
+                    <h5>CSS</h5>
+                </Col>
+                <Col md={4} className='skillsList'>
+                    <FaBootstrap />
+                    <h5>Bootstrap</h5>
+                </Col>
+                <Col md={4} className='skillsList'>
+                    <SiSequelize />
+                    <h5>Sequelize</h5>
+                </Col>
+                <Col md={4} className='skillsList'>
+                    <SiPostman />
+                    <h5>Postman</h5>
                 </Col>
             </Row>
         </Container>
