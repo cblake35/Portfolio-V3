139c4e283fe8a970790fa7cdfad2cd64589628bc
Chris Blake <cblake2335@gmail.com>
2021-11-03 19:58:47 -0400

    Added minor animation for about me image. Started work section. Also added basic page structure, styling and images for each project.

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import neighborhub from '../assets/neighborhub.png';
+import ecommerce from '../assets/ecommerce.png';
+import flixdb from '../assets/flixdb.png';
 
 function Work() {
 
 
     return (
-        <>
-            <h1>This is the work page.</h1>
-        </>
+        <Container>
+            <Row className='workTitleWrapper'>
+                <Col className='workTitle'>
+                    <h1>Featured<span> Work</span></h1>
+                </Col>
+            </Row>
+            <Row className='workListWrapper'>
+                <Col md={4} className='workList'>
+                    <img src={neighborhub} alt='NeighborHub App'/>
+                </Col>
+                <Col md={4} className='workList'>
+                <img src={ecommerce} alt='Ecommerce Website'/>
+                </Col>
+                <Col md={4} className='workList'>
+                <img src={flixdb} alt='FlixDB App'/>
+                </Col>
+            </Row>
+            <Row className='miniappTitleWrapper'>
+                <Col className='miniappTitle'>
+                    <h2>Mini<span> Apps</span></h2>
+                </Col>
+            </Row>
+            <Row className='miniappWrapper'>
+                <Col md={4} className='miniappList'>
+                    mini app
+                </Col>
+                <Col md={4} className='miniappList'>
+                    mini app
+                </Col>
+                <Col md={4} className='miniappList'>
+                    mini app
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
