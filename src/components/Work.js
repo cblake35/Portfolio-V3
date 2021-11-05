d4dbe2c368cb5fcf9b5a78b09bdde92f5b3ac384
Chris Blake <cblake2335@gmail.com>
2021-11-04 20:15:22 -0400

    Added hover elements for each project. Added styling for featured work section.

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,16 +16,25 @@ function Work() {
             </Row>
             <Row className='workListWrapper'>
                 <Col md={4} className='workList'>
-                    <img src={neighborhub} alt='NeighborHub App'/>
+                    <img src={neighborhub} alt='NeighborHub App' />
+                    <div className='projectTitleOverlay'>
+                        <p>NeighborHUB</p>
+                    </div>
                 </Col>
                 <Col md={4} className='workList'>
-                <img src={ecommerce} alt='Ecommerce Website'/>
+                    <img src={ecommerce} alt='Ecommerce Website' />
+                    <div className='projectTitleOverlay'>
+                        <p>Bedder Way Co</p>
+                    </div>
                 </Col>
                 <Col md={4} className='workList'>
-                <img src={flixdb} alt='FlixDB App'/>
+                    <img src={flixdb} alt='FlixDB App' />
+                    <div className='projectTitleOverlay'>
+                        <p>FlixDB</p>
+                    </div>
                 </Col>
             </Row>
-            <Row className='miniappTitleWrapper'>
+            {/* <Row className='miniappTitleWrapper'>
                 <Col className='miniappTitle'>
                     <h2>Mini<span> Apps</span></h2>
                 </Col>
@@ -40,7 +49,7 @@ function Work() {
                 <Col md={4} className='miniappList'>
                     mini app
                 </Col>
-            </Row>
+            </Row> */}
         </Container>
     )
 }
