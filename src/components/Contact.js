2acf116a5e11fe986be60e6e36a20b4cbda18624
Chris Blake <cblake2335@gmail.com>
2021-11-06 20:59:25 -0400

    Finished adding content to about me section. Finished initial content update to contact me page. Further styling and functionality update needed.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,53 @@
 import React from "react";
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import { RiMailSendLine } from 'react-icons/ri';
+import { FiSend } from 'react-icons/fi';
 
 function Contact() {
 
 
     return (
-        <>
-            <h1>This is the contact page.</h1>
-        </>
+        <Container id='contactPageContainer'>
+            <Row className='contactTitleWrapper'>
+                <Col className='contactTitle'>
+                    <h1>Get In<span> Touch</span></h1>
+                </Col>
+            </Row>
+            <Row className='contactContentWrapper'>
+                <Col md={3} className='contactInfo'>
+                    <h3>Let's Connect</h3>
+                    <p>I'm always looking for opportunities to work on new projects,
+                        discuss creative ideas, or network. Feel free to get in touch
+                        with me.
+                    </p>
+                    <p><RiMailSendLine /> cblake2335@gmail.com</p>
+                </Col>
+                <Col md={9} className='contactForm'>
+                    <Form as={Row} action='https://formspree.io/f/mgerjrng' method='POST'>
+                        <Form.Group md={6} as={Col} className='contactFormName mb-3' controlId='formcontrol1'>
+                            <Form.Label>FULL NAME</Form.Label>
+                            <Form.Control type='email' placeholder='First and Last' required />
+                        </Form.Group>
+                        <Form.Group md={6} as={Col} className='contactFormEmail mb-3' controlId='formcontrol2'>
+                            <Form.Label>EMAIL</Form.Label>
+                            <Form.Control type='email' placeholder='youremail@example.com' required />
+                        </Form.Group>
+                        <Form.Group md={12} as={Col} className='contactFormMsg mb-3' controlId='formcontrol3'>
+                            <Form.Label>MESSAGE</Form.Label>
+                            <Form.Control as='textarea' rows={4} placeholder='Type message here' required />
+                        </Form.Group>
+                        <Form.Group md ={12} as={Col}>
+                            <Col>
+                                <button type='submit'>
+                                    <span className='contactMeSpan'>Contact Me</span>
+                                    <span className='contactMeSvg'><FiSend /></span>
+                                </button>
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
--- src/components/Work.js
+++ src/components/Work.js
@@ -3,6 +3,7 @@ import { Container, Row, Col, Modal } from 'react-bootstrap';
 import neighborhub from '../assets/neighborhub.png';
 import ecommerce from '../assets/ecommerce.png';
 import flixdb from '../assets/flixdb.png';
+import {VscGithub} from 'react-icons/vsc'
 
 function Work() {
     const [showOne, setShowOne] = useState(false);
@@ -83,47 +84,129 @@ const MyModals = (props) => {
         <>
             <Modal show={props.showOne} onHide={props.handleCloseOne}>
                 <Modal.Header closeButton>
-                    <Modal.Title>NeighborHUB</Modal.Title>
+                    <Modal.Title className='modalTitle'>NeighborHUB</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <button onClick={(e) => props.handleCloseOne()}>
-                        Close
-                    </button>
-                    <button onClick={(e) => props.handleCloseOne()}>
-                        Save Changes
-                    </button>
-                </Modal.Footer>
+                <Modal.Body className='modalBody'>
+                        <img className='modalImg' src={neighborhub} alt='NeighborHub App' />
+                        <Row className='siteDescriptionWrapper'>
+                            <Col className='siteDescription'>
+                                <h6>Info:</h6>
+                                <p>A full-stack web app for residential communities. The purpose of this app is to create
+                                    a localized social hub for communities to interact in. It also provides an intuitive
+                                    ticketing system for residents and allows administrators to manage, track, and resolve each submitted ticket. 
+                                    This app features full CRUD functionality and role-based access control.
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row className='modalStackWrapper'>
+                            <Col className='stackList'>
+                                <h6>Stack:</h6>
+                                <ul>
+                                    <li>React</li>
+                                    <li>PostgreSQL</li>
+                                    <li>Node</li>
+                                    <li>Express</li>
+                                    <li>Heroku</li>
+                                    <li>Bootstrap</li>
+                                    <li>Ticketmaster API</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                        <Row className='livesiteWrapper'>
+                            <Col>
+                                <h6>Site: </h6>
+                                <a className='livesite' href='https://neighborhub-client.herokuapp.com/' target='_blank' rel='noreferrer'> NeighborHUB</a>
+                                <div className='demoLoginWrapper'>
+                                    <h6>Demo Login(Tenant/Admin)</h6>
+                                    <p>Username: test1</p>
+                                    <p>Password: Password1</p>
+                                </div>
+                                <a className='githubRepo' href='https://github.com/cblake35/NeighborHUB-Client' target='_blank' rel='noreferrer'><VscGithub /></a>
+                            </Col>
+                        </Row>
+                </Modal.Body>
+                <Modal.Footer> </Modal.Footer>
             </Modal>
 
             <Modal show={props.showTwo} onHide={props.handleCloseTwo}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Bedder Way Co</Modal.Title>
+                    <Modal.Title className='modalTitle'>Bedder Way Co</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <button onClick={(e) => props.handleCloseTwo()}>
-                        Close
-                    </button>
-                    <button onClick={(e) => props.handleCloseTwo()}>
-                        Save Changes
-                    </button>
-                </Modal.Footer>
+                <Modal.Body className='modalBody'>
+                        <img className='modalImg' src={ecommerce} alt='NeighborHub App' />
+                        <Row className='siteDescriptionWrapper'>
+                            <Col className='siteDescription'>
+                                <h6>Info:</h6>
+                                <p>An ecommerce website for Murphy Beds. The site is built using the Shopify CMS and custom HTML, CSS, and JavaScript.
+                                    Multiple plugins are also utilized to optimize the site for a better customer experience.
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row className='modalStackWrapper'>
+                            <Col className='stackList'>
+                                <h6>Stack:</h6>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>Shopify</li>
+                                    <li>Bootstrap</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                        <Row className='livesiteWrapper'>
+                            <Col>
+                                <h6>Site: </h6>
+                                <a className='livesite' href='https://bedderwayco.com/' target='_blank' rel='noreferrer'> Bedder Way Co</a>
+                            </Col>
+                        </Row>
+                </Modal.Body>
+                <Modal.Footer> </Modal.Footer>
             </Modal>
-            
+
             <Modal show={props.showThree} onHide={props.handleCloseThree}>
                 <Modal.Header closeButton>
-                    <Modal.Title>FlixDB</Modal.Title>
+                    <Modal.Title className='modalTitle'>FlixDB</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <button onClick={(e) => props.handleCloseThree()}>
-                        Close
-                    </button>
-                    <button onClick={(e) => props.handleCloseThree()}>
-                        Save Changes
-                    </button>
-                </Modal.Footer>
+                <Modal.Body className='modalBody'>
+                        <img className='modalImg' src={flixdb} alt='NeighborHub App' />
+                        <Row className='siteDescriptionWrapper'>
+                            <Col className='siteDescription'>
+                                <h6>Info:</h6>
+                                <p>A full-stack web app for browsing and reviewing popular movie titles. The app utilizes a PostgreSQL database 
+                                    to allow users to bookmark movies, search reviews from other users, and view their past reviews. This app feautures
+                                    full CRUD functionality.
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row className='modalStackWrapper'>
+                            <Col className='stackList'>
+                                <h6>Stack:</h6>
+                                <ul>
+                                    <li>React</li>
+                                    <li>PostgreSQL</li>
+                                    <li>Node</li>
+                                    <li>Express</li>
+                                    <li>Heroku</li>
+                                    <li>Bootstrap</li>
+                                    <li>Movie Database API</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                        <Row className='livesiteWrapper'>
+                            <Col>
+                                <h6>Site: </h6>
+                                <a className='livesite' href='https://flixdb-client.herokuapp.com/' target='_blank' rel='noreferrer'> FlixDB</a>
+                                <div className='demoLoginWrapper'>
+                                    <h6>Demo Login</h6>
+                                    <p>Username: test1</p>
+                                    <p>Password: Password1</p>
+                                </div>
+                                <a className='githubRepo' href='https://github.com/cblake35/FlixDB-Client' target='_blank' rel='noreferrer'><VscGithub /></a>
+                            </Col>
+                        </Row>
+                </Modal.Body>
+                <Modal.Footer> </Modal.Footer>
             </Modal>
         </>
     )
