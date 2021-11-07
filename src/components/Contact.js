c1152ad23c2994beb999e152db842d00e172a53e
Chris Blake <cblake2335@gmail.com>
2021-11-07 14:06:31 -0500

    Finished contact me page with working form. Also added success identifier if email is sent successfully.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import { RiMailSendLine } from 'react-icons/ri';
 import { FiSend } from 'react-icons/fi';
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
-
+    const [state, handleSubmit] = useForm('mgerjrng');
 
     return (
         <Container id='contactPageContainer'>
@@ -23,27 +24,44 @@ function Contact() {
                     <p><RiMailSendLine /> cblake2335@gmail.com</p>
                 </Col>
                 <Col md={9} className='contactForm'>
-                    <Form as={Row} action='https://formspree.io/f/mgerjrng' method='POST'>
-                        <Form.Group md={6} as={Col} className='contactFormName mb-3' controlId='formcontrol1'>
-                            <Form.Label>FULL NAME</Form.Label>
-                            <Form.Control type='email' placeholder='First and Last' required />
-                        </Form.Group>
-                        <Form.Group md={6} as={Col} className='contactFormEmail mb-3' controlId='formcontrol2'>
-                            <Form.Label>EMAIL</Form.Label>
-                            <Form.Control type='email' placeholder='youremail@example.com' required />
-                        </Form.Group>
-                        <Form.Group md={12} as={Col} className='contactFormMsg mb-3' controlId='formcontrol3'>
-                            <Form.Label>MESSAGE</Form.Label>
-                            <Form.Control as='textarea' rows={4} placeholder='Type message here' required />
-                        </Form.Group>
-                        <Form.Group md ={12} as={Col}>
-                            <Col>
-                                <button type='submit'>
-                                    <span className='contactMeSpan'>Contact Me</span>
-                                    <span className='contactMeSvg'><FiSend /></span>
-                                </button>
-                            </Col>
-                        </Form.Group>
+                    <Form onSubmit={handleSubmit}>
+                        <Row>
+                            <Form.Group md={4} as={Col} className='contactFormName mb-3' controlId='formcontrol1'>
+                                <Form.Label>FULL NAME</Form.Label>
+                                <Form.Control name='fullname' placeholder='First and Last' required />
+                                <ValidationError prefix='Full Name' field='fullname' errors={state.errors} />
+                            </Form.Group>
+                            <Form.Group md={4} as={Col} className='contactFormEmail mb-3' controlId='formcontrol2'>
+                                <Form.Label>EMAIL</Form.Label>
+                                <Form.Control type='email' name='email' placeholder='youremail@example.com' required />
+                                <ValidationError prefix='Email' field='email' errors={state.errors} />
+                            </Form.Group>
+                            <Form.Group md={4} as={Col} className='contactFormSubject mb-3' controlId='formcontrol3'>
+                                <Form.Label>SUBJECT</Form.Label>
+                                <Form.Control  name='subject' placeholder='Subject' required />
+                                <ValidationError prefix='Subject' field='subject' errors={state.errors} />
+                            </Form.Group>
+                            <Form.Group md={12} as={Col} className='contactFormMsg mb-3' controlId='formcontrol4'>
+                                <Form.Label>MESSAGE</Form.Label>
+                                <Form.Control as='textarea' name='message' rows={4} placeholder='Type message here' required />
+                                <ValidationError prefix='Message' field='message' errors={state.errors} />
+                            </Form.Group>
+                            <Form.Group md={12} as={Col}>
+                                <Col>
+                                    <button type='submit' disabled={state.submitting}>
+                                        <span className='contactMeSpan'>Contact Me</span>
+                                        <span className='contactMeSvg'><FiSend /></span>
+                                    </button>
+                                </Col>
+                            </Form.Group>
+                            {state.succeeded ?
+                                <Col className='emailSuccess' md={12} >
+                                    <p>Email Sent!</p>
+                                </Col>
+                                :
+                                undefined
+                            }
+                        </Row>
                     </Form>
                 </Col>
             </Row>
